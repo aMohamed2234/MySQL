5124388c3685ddeb22a9fe9494fed0691f3d52ce
ABDIRAUF <user@Abdirauf.ABDIRAUF>
2024-11-19 15:27:44 +0300

    created

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,6 +2,6 @@ module.exports= {
     HOST: process.env.HOST,
     USER: process.env.USER,
     PASSWORD: process.env.PASSWORD,
-    DB: process.env.,
+    DB: process.env.DB,
     dialect: 'mysql'
 }
